ace02d50655bf319bf3fa7522d27ab9bc39d313b
Gabriel Wilson <47883480+g-i-wilson@users.noreply.github.com>
2021-07-01 14:58:40 -0500

    working HTTP frontend for pigpiod, using paddle!

--- WebPiGPIO.java
+++ WebPiGPIO.java
@@ -3,11 +3,10 @@ import paddle.*;
 
 public class WebPiGPIO {
 
-	private static String pigpiodServer = "localhost";
 	private static int pigpiodPort = 8888;
 
 	public static void main (String[] args) {
-		ServerState state = new ServerStateWebPiGPIO( pigpiodServer, pigpiodPort );
+		ServerState state = new ServerStateWebPiGPIO( args[0], pigpiodPort );
 		Server gui = new ServerHTTP( state, 7000, "Web GUI for pigpiod" );
 	}
 
@@ -37,37 +36,32 @@ class ServerStateWebPiGPIO extends ServerState {
 	public void respond ( InboundHTTP http ) {
 		try { // try to send request to pigpiod
 		
-			System.out.println( http.request().firstLine() );
 			String reqData = new String(http.request().data());
 			String[] pigpiodParams = reqData.split(",");
 			Bytes pigpiodBytes = new Bytes( new byte[4*pigpiodParams.length] );
 			for (int i=0; i<pigpiodParams.length; i++) {
-				pigpiodBytes.writeIntBE( Integer.parseInt(pigpiodParams[i]), i*4, 4 );
+				//pigpiodBytes.writeIntBE( Integer.parseInt(pigpiodParams[i]), i*4, 4 );
+				pigpiodBytes.writeIntLE( Integer.parseInt(pigpiodParams[i]), i*4, 4 );
 			}
 			
-			if (http.request().path().toLowerCase().equals("/gpio")) {
-				String pigpiodResponse = (new OutboundTCP(
-					this,
-					pigpiodServer,
-					pigpiodPort,
-					reqData,
-					pigpiodBytes.bytes(),
-					new byte[pigpiodBytes.size()],
-					-1,
-					true
-				))
-				.receive()
-				.text();
-				http.response().setBody(
-					"<h1>pigpiod response:</h1>\n<br>\n"+
-					pigpiodResponse+"\n<br>\n"
-				);
-			} else {
-				http.response().setBody(
-					"<h1>Command was not recognized:</h1>\n<br>\n"+
-					"'"+http.request().path().toLowerCase()+"'\n<br>\n"
-				);
-			}
+			String pigpiodResponse = (new OutboundTCP(
+				this,
+				pigpiodServer,
+				pigpiodPort,
+				"Sending '"+reqData+"' to pigpiod...",
+				pigpiodBytes.bytes(),
+				new byte[pigpiodBytes.size()],
+				-1,
+				true
+			))
+			.timeout(1000)
+			.receive()
+			.hex();
+			
+			http.response().setBody(
+				"<h1>pigpiod response:</h1>\n<br>\n"+
+				"hex: '"+pigpiodResponse+"'\n<br>\n"
+			);
 			
 		} catch (Exception e) {
 				http.response().setBody(
